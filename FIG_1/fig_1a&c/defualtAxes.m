7f88fdbc32245eec23c1e13c8903353c92197850
Haibo Tang <98083672+Haibo-Tang@users.noreply.github.com>
2023-11-15 19:37:04 +0800

    Git LFS commit

--- FIG_1/fig_1a&c/defualtAxes.m
+++ FIG_1/fig_1a&c/defualtAxes.m
@@ -1,17 +1,3 @@
-function defualtAxes
-ax=gca;hold on;box on
-ax.XGrid='on';
-ax.YGrid='on';
-ax.XMinorTick='off';
-ax.YMinorTick='off';
-ax.LineWidth=2;
-ax.GridLineStyle='-.';
-ax.FontName='Cambria';
-ax.FontSize=25;
-axis equal
-set(gca,'linewidth',3,'box','on','fontsize',35)
-xlim([117.5 120.5])
-ylim([19.5 22.5])
-xticklabels([118 119 120])
-yticklabels([20 21 22])
-end
+version https://git-lfs.github.com/spec/v1
+oid sha256:55f480047916ff9012c27816974c34e8de6d8b7cad20d94fabba73255449c3e0
+size 360
